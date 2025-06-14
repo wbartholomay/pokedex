b81d876896f6f09cea80deff1f81cce611c62e51
wbartholomay <willbartholomay@gmail.com>
2025-06-13 20:36:21 -0400

    added rEPL

--- repl.go
+++ repl.go
@@ -2,10 +2,28 @@ package main
 
 import (
 	"strings"
+	"bufio"
+	"fmt"
+	"os"
 )
 
 func cleanInput(text string) []string {
 	text = strings.ToLower(text)
 	substrings := strings.Fields(text)
 	return substrings
+}
+
+func startRepl() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+
+		t := scanner.Text()
+		input := cleanInput(t)
+		if len(input) == 0 { continue }
+
+		fmt.Println("Your command was: " + input[0])
+	}
 }
