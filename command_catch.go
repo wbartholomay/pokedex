cb86c5061088ed6aa9d14372d15023fb883494e1
Will Bartholomay <william.bartholomay@uconn.edu>
2025-06-14 14:20:04 -0400

    added catching functionality

--- command_catch.go
+++ command_catch.go
@@ -3,6 +3,7 @@ package main
 import (
 	"errors"
 	"fmt"
+	"math/rand/v2"
 )
 
 func CommandCatch(cfg *config, params ...string) error {
@@ -10,14 +11,22 @@ func CommandCatch(cfg *config, params ...string) error {
 		return errors.New("must provide a pokemon. Example usage: catch <pokemon-name>")
 	}
 
-	url := "https://pokeapi.co/api/v2/pokemon/" + params[0]
+	pokemonName := params[0]
+
+	url := "https://pokeapi.co/api/v2/pokemon/" + pokemonName
 	pokemonRes, err := cfg.pokeapiClient.GetPokemonDetails(url)
 	if err != nil {
 		return err
 	}
 
 	exp := pokemonRes.BaseExperience
-	fmt.Println(exp)
+	fmt.Printf("Throwing a Pokeball at %s\n", pokemonName)
+	
+	if rand.IntN(609) > exp / 2 {
+		fmt.Printf("%s was caught!\n", pokemonName)
+	} else {
+		fmt.Printf("%s escaped!\n", pokemonName)
+	}
 
 	return nil
 
